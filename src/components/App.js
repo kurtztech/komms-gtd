637f54f8706516a15c68bddb910aed3708999e30
kurtztech <sam@kurtztech.com>
2019-04-25 13:32:37 -0600

    reduce interval time for calculating a stale inbox

--- src/components/App.js
+++ src/components/App.js
@@ -59,7 +59,7 @@ class App extends Component {
     this.calcStaleInbox();
     this.staleRef = setInterval(() => {
       this.calcStaleInbox();
-    }, 1000);
+    }, 100);
   }
 
   componentWillUnmount() {
